b69e8e42bed00b1458503c9a3366dbf33f50d1f3
spaget-spaget <oliverlimyk@gmail.com>
2025-07-09 10:20:56 +0800

    Adding a cat and putting in animations
    its free from the asset store. also, it moves up and down. managed to restrict it to the 4 lanes, and have it move up and down rather than teleporting. currently trying to add a jump and crouch feature.

---
+++ Project2/Assets/Scripts/CatMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int CurrentPosition = 2;
+    private Vector3 targetPosition;
+    public float moveSpeed = 10.0f;
+    private 
+    void Start()
+    {
+        targetPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        // If we're not at the target yet, don't allow new input
+        if (transform.position != targetPosition)
+            return;
+
+        if (CurrentPosition < 3)
+        {
+            if (Input.GetKey("w"))
+            {
+                MoveCatUp();
+            }
+        }
+        if (CurrentPosition > 0)
+        {
+            if (Input.GetKey("s"))
+            {
+                MoveCatDown();
+            }
+        }
+    }
+    void MoveCatUp()
+    {
+        targetPosition += new Vector3(0, 3, 0);
+        CurrentPosition += 1;
+    }
+    void MoveCatDown()
+    {
+        targetPosition += new Vector3(0, -3, 0);
+        CurrentPosition -= 1;
+    }
+    void CatJump()
+    {
+        
+    }
+}
